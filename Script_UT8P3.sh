8224ff4774f3ed8002c71cdc7b1000f47761dd3c
Dysforic <94018158+Dysforic@users.noreply.github.com>
2023-02-19 22:11:09 +0100

    Update Script_UT8P3.sh

--- Script_UT8P3.sh
+++ Script_UT8P3.sh
@@ -2,7 +2,7 @@
 
 # Realiza un script que indique si el fichero pasado por párametro
 # existe o no existe. Además debe indicar se si trata de un
-# archvio regular o un directorio
+# archivo regular o un directorio
 
 	if [[ -f $1 ]]; then
 
